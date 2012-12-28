c06017202e7ea0bcc0d7fea4649bfd6aa01a205d
Jooyung Han <jooyung.han@gmail.com>
2012-12-28 16:10:19 +0900

    Added TextViewDriver (similar to windowlicker)

--- test/src/com/jooyunghan/auctionsniper/test/AuctionSniperDriver.java
+++ test/src/com/jooyunghan/auctionsniper/test/AuctionSniperDriver.java
@@ -1,10 +1,13 @@
 package com.jooyunghan.auctionsniper.test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+
+import org.hamcrest.Matcher;
+
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.os.SystemClock;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -14,37 +17,48 @@ import com.jooyunghan.auctionsniper.R;
 public class AuctionSniperDriver extends Solo {
 	private int timeout;
 
-	public AuctionSniperDriver(Instrumentation inst, Activity activity, int timeout) {
+	public AuctionSniperDriver(Instrumentation inst, Activity activity,
+			int timeout) {
 		super(inst, activity);
 		this.timeout = timeout;
 		assertCurrentActivity("activity not launched", MainActivity.class);
 	}
 
 	public void showsSniperStatus(String statusText) {
-		waitForText(R.id.status, statusText);
-		Log.d("han", "showsSniperStatus:" + statusText);
+		new TextViewDriver(this, R.id.status).hasText(equalTo(statusText), timeout);
 	}
 
-	private void waitForText(int resId, String statusText) {
-		final long endTime = SystemClock.uptimeMillis() + timeout;
+	static class TextViewDriver {
+		private Solo solo;
+		private int resId;
 
-		TextView view = (TextView) getView(resId);
-		String lastText = view.getText().toString();
-
-		while (SystemClock.uptimeMillis() < endTime) {
-			lastText = view.getText().toString();
-			if (lastText.equals(statusText)) {
-				return;
-			}
-			sleep();
+		TextViewDriver(Solo solo, int resId) {
+			this.solo = solo;
+			this.resId = resId;
 		}
-		
-		assertThat(lastText, equalTo(statusText));
-	}
 
-	public void sleep() {
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException ignored) {}
+		public void hasText(Matcher<? super String> matcher, int timeout) {
+			final long endTime = SystemClock.uptimeMillis() + timeout;
+
+			TextView view = (TextView) solo.getView(resId);
+			String lastText = view.getText().toString();
+
+			while (SystemClock.uptimeMillis() < endTime) {
+				lastText = view.getText().toString();
+				if (matcher.matches(lastText)) {
+					return;
+				}
+				sleep();
+			}
+
+			assertThat(lastText, matcher);
+		}
+
+		static private void sleep() {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ignored) {
+			}
+		}
 	}
 }
